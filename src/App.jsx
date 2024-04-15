626c0cc72b7da27e6dd102049a2f2fed91dae37b
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-15 13:47:02 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function getRandomVal(val) {
 }
 
 function Header() {
+  const description = reactDescriptions[getRandomVal(2)]
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[getRandomVal(2)]} React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
