884cda49ed000e4e68419aeeebce6b1c5f9f5e35
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-15 13:44:41 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function Header() {
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {Fundamental} React concepts you will need for almost any app you are
+        {reactDescriptions[getRandomVal(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
