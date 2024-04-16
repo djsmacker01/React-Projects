0656cc6d8a4eeac2fc4283a19351600072a8b70f
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-16 13:43:23 +0100

    commit

--- src/data.js
+++ src/data.js
@@ -2,3 +2,4 @@ import componentsImg from './assets/components.png'
 import propsImage from './assets/config.png'
 import jxImg from './assets/jsx-ui.png'
 import stateImg from './assets/state-mgmt.png'
+
