d3b2f58e3015389884750ee1f37d562114e35587
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-16 13:36:36 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function Header() {
 function CoreConcept(props) {
   return (
     <li>
-      <img src={props.image} alt="" />
+      <img src={props.image} alt={props.title} />
       <h3>{ props.title}</h3>
       <p>{props.description}</p>
     </li>
