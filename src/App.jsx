ddc3a746a45f8638d183cb9a5b6d0a4a12bced78
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-16 17:12:03 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,9 @@ import componentImg from './assets/components.png'
 
 
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core ']
 
 
-function getRandomVal(val) {
-  return Math.floor(Math.random() *(val + 1))
-}
 
-
-function Header() {
-  const description = reactDescriptions[getRandomVal(2)]
-  return (
-    <header>
-      <img src={headerImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
 function CoreConcept({image, description,title}) {
   return (
