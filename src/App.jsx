9506926ff01735f8c427db124ea35a4ca32033df
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-16 14:33:19 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} /> */}
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
