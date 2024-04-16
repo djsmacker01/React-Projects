f0de22c7d132a81f4d3ced65bcb43c44a9bc7ebc
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-16 13:25:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,13 @@ function App() {
     <div>
      <Header/>
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept/>
+
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
