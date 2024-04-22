2dc223f652fb1eebf41545e0c3f965b12990f991
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-22 21:11:27 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -42,6 +42,7 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
+          {!selectedTopic &&}
           <div id="tab-content">
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
