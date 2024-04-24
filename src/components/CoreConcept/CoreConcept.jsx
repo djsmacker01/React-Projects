10280f135c2dab9875ef7a852cd98f89f90f8435
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-24 18:17:46 +0100

    commit

--- src/components/CoreConcept/CoreConcept.jsx
+++ src/components/CoreConcept/CoreConcept.jsx
@@ -1,7 +1,6 @@
 export default function CoreConcept({ image, description, title,  }) {
   return (
     <li>
-      
       <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
