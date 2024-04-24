f4050baf28bfc4442a0b1511a83599ec23fac6b5
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-24 18:19:27 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
+          
           <ul>
             {CORE_CONCEPTS.map((conceptItem) => (
               <CoreConcept key={conceptItem.title} {...conceptItem} />
