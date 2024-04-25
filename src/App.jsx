cf7cd4e385134a7c5898a7675160bb6dd60eb81a
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-25 16:19:06 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { CORE_CONCEPTS } from "./data.js";
+
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
 import { EXAMPLES } from "./dataContent.js";
