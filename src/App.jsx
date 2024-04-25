99684ca9840d61ad50737440b0f13e3e42cd29cf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-25 16:15:44 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,24 +29,7 @@ function App() {
     <div>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-
-            {/* <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
-          </ul>
-        </section>
+        
         <section id="examples">
           <h2>Example</h2>
           <menu>
