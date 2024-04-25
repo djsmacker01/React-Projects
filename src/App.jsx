d7953eefd14f94e2e971adf384e75b02986d8f15
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-25 16:30:34 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,42 +29,7 @@ function App() {
       <Header />
       <main>
         <CoreConcepts /> 
-        <section id="examples">
-          <h2>Example</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleClick("components")}>
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleClick("jsx")}>
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleClick("props")}>
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleClick("state")}>
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-          {/* {!selectedTopic && <p>Please Select a topic</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )} */}
-        </section>
+        
         {/* <h2>Time to get started!</h2> */}
       </main>
     </div>
