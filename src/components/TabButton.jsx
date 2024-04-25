cfcf2e7584ac8b831f19382b3dd1313b77725805
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-04-25 17:19:05 +0100

    commit

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props}) {
   
    
   return (
       <li>
-          <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
+          <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
       </li>
   )
 }
