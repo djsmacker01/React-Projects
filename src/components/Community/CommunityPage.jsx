f7572bb81c868bd61537bb4867a1845fb0fe6c4f
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-22 12:21:00 +0100

    add another component

---
+++ src/components/Community/CommunityPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+//place holder post for data
+const demoPost = [
+  {
+    id: 1,
+    user: "user1",
+    comment: "First comment",
+    time: "1 hour ago",
+    upvotes: 10,
+    downvotes: 2,
+  },
+  {
+    id: 2,
+    user: "user3",
+    comment: "user3 comment",
+    time: "3 hour ago",
+    upvotes: 12,
+    downvotes: 4,
+  },
+];
+
+const CommunityPage = () => {
+    
+
+  return (
+    <div>CommunityPage</div>
+  )
+}
+
+export default CommunityPage
