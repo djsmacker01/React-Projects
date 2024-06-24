807d6b363c87de988a957551d043c560f9c333d0
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-24 13:39:19 +0100

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import App1 from './components/App1.jsx'
-// import App3 from './App3.jsx'
+// import App1 from './components/App1.jsx'
+import App3 from './App3.jsx'
 import './index.css'
 import './components/Header/Header.css'
 
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     <App1 />
-    {/* <App3 /> */}
+    <App3 />
   </React.StrictMode>,
 )
