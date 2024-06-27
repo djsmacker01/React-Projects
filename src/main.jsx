2f294312da82d02a29193ad7ee291afa39e58dcf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-06-27 14:31:14 +0100

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import App1 from './components/App1.jsx'
-import App3 from './App3.jsx'
+// import App3 from './App3.jsx'
 import './index.css'
 import './components/Header/Header.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <App1 />
-    <App3 />
+    <App />
+    {/* <App1 />
+    <App3 /> */}
   </React.StrictMode>,
 )
