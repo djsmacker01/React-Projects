4fb10033a2b4392cc788747de0627726c5740fe7
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-10 22:28:34 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App1 from './App1.jsx'
-// import App1 from './components/App1.jsx'
-// import App3 from './App3.jsx'
 import './index.css'
 import './components/Header/Header.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App1 />
-    {/* <App1 />
-    <App3 /> */}
-  </React.StrictMode>,
-)
+    <Router>
+      <App1 />
+    </Router>
+  </React.StrictMode>
+);
