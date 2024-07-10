51f63f72cc5b87c9cfeaffa2f4bc99e328277f46
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-10 22:28:24 +0100

    update app js

--- src/App1.jsx
+++ src/App1.jsx
@@ -1,19 +1,82 @@
+// import React, { useState } from "react";
+// import { Route, Routes, Link, useNavigate } from "react-router-dom";
+// import CreateCommunity from "./components/CreateCommunity";
+// import CommunityPage from "./components/Community/CommunityPage";
+
+// const App1 = () => {
+//   const [communities, setCommunities] = useState([]);
+//   const navigate = useNavigate();
+
+//   const handleCreateCommunity = (community) => {
+//     setCommunities([...communities, community]);
+//     navigate(`/community/${community.title}`);
+//   };
+
+//   return (
+//     <div>
+//       <Link to="/create-community">
+//         <button style={buttonStyles}>+</button>
+//       </Link>
+//       <Routes>
+//         <Route
+//           path="/create-community"
+//           element={<CreateCommunity onCreate={handleCreateCommunity} />}
+//         />
+//         <Route
+//           path="/community/:title"
+//           element={<CommunityPage communities={communities} />}
+//         />
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// const buttonStyles = {
+//   fontSize: "24px",
+//   width: "50px",
+//   height: "50px",
+//   borderRadius: "50%",
+//   backgroundColor: "#f90",
+//   color: "#fff",
+//   border: "none",
+//   cursor: "pointer",
+//   position: "fixed",
+//   bottom: "20px",
+//   right: "20px",
+// };
+
+// export default App1;
+
+// App1.js
 import React, { useState } from "react";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import CreateCommunity from "./components/CreateCommunity";
+import CommunityPage from "./components/Community/CommunityPage";
 
-const App = () => {
-  const [showForm, setShowForm] = useState(false);
+const App1 = () => {
+  const [communities, setCommunities] = useState([]);
+  const navigate = useNavigate();
 
-  const handleButtonClick = () => {
-    setShowForm(true);
+  const handleCreateCommunity = (community) => {
+    setCommunities([...communities, community]);
+    navigate(`/community/${community.title}`);
   };
 
   return (
     <div>
-      <button onClick={handleButtonClick} style={buttonStyles}>
-        +
-      </button>
-      {showForm && <CreateCommunity />}
+      <Link to="/create-community">
+        <button style={buttonStyles}>+</button>
+      </Link>
+      <Routes>
+        <Route
+          path="/create-community"
+          element={<CreateCommunity onCreate={handleCreateCommunity} />}
+        />
+        <Route
+          path="/community/:title"
+          element={<CommunityPage communities={communities} />}
+        />
+      </Routes>
     </div>
   );
 };
@@ -32,4 +95,5 @@ const buttonStyles = {
   right: "20px",
 };
 
-export default App;
+export default App1;
+
