10357e77e90ded9e338f9f18fa9b3e74469cd74d
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-25 17:40:46 +0100

    commit

---
+++ src/App3.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import CommunityPage from "./components/CommunityPage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <CommunityPage />
+    </div>
+  );
+};
+
+export default App;
