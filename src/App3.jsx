b268aa09d5d9abc38215d44b2437e69034fe77bf
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-25 18:29:51 +0100

    new commit

--- src/App3.jsx
+++ src/App3.jsx
@@ -1,12 +1,15 @@
 // src/App.js
 import React from "react";
-import CommunityPage from "./components/CommunityPage";
+import CommunityPage from "./components/Community/CommunityPage";
 import "./App.css";
+import CreateCommunity from "./components/CreateCommunity";
 
 const App = () => {
   return (
     <div className="App">
-      <CommunityPage />
+          {/* <CommunityPage /> */}
+          {/* <CreateCommunity /> */}
+          <CommunityPage/>
     </div>
   );
 };
