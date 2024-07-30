abaf687d65cf1b1335e50c9544a4e28474e15f34
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-07-30 16:22:08 +0100

    update main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App3 from './App3.jsx'
+import App4 from './App4.jsx'
 import './index.css'
 import './components/Header/Header.css'
 import { BrowserRouter as Router } from 'react-router-dom'
@@ -9,7 +9,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App3 />
+      <App4 />
     </Router>
   </React.StrictMode>
 );
