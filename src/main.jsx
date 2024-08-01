95c7e9386b6cd5d8cb39480e59524928c24b7d51
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-08-01 18:15:10 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App4 from './App4.jsx'
+// import App4 from './App4.jsx'
 import './index.css'
 import './components/Header/Header.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import CommunityPage from './components/CommunityPage'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App4 />
+      {/* <App4 /> */}
+      <CommunityPage/>
     </Router>
   </React.StrictMode>
 );
