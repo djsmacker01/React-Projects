87e004a2ed243563012618e4f8536090dff31321
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-08-01 18:15:20 +0100

    commit

---
+++ src/components/Community.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Post from "../components/Post";
+import JoinButton from "../components/JoinButton";
+
+const Community = ({
+  community,
+  onJoin,
+  posts,
+  onLike,
+  onComment,
+  onCommentUpvote,
+  onCommentDelete,
+}) => (
+  <div style={{ border: "1px solid black", padding: "10px", margin: "10px" }}>
+    <h2>
+      {community.name}
+      {!community.joined && <JoinButton onJoin={() => onJoin(community.id)} />}
+    </h2>
+    {community.joined && <button>Post</button>}
+    {posts.map((post) => (
+      <Post
+        key={post.id}
+        post={post}
+        canComment={!community.joined}
+        onLike={onLike}
+        onComment={onComment}
+        onCommentUpvote={onCommentUpvote}
+        onCommentDelete={onCommentDelete}
+      />
+    ))}
+  </div>
+);
+
+export default Community;
