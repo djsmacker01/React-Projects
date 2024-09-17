c0ebecc4763b84a5c9a69dbcbd20ea1d398e82df
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-09-17 20:24:23 +0100

    commit

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import './CreatePost'
+// import './CreatePost.css'
 
 const CreatePost = () => {
   const [activeTab, setActiveTab] = useState("Text");
