886eca2a1fd6e05d939d73d37520bd06ac4d81c8
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-22 17:50:17 +0000

    commit

--- src/components/JoinButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const JoinButton = ({ onJoin }) => (
-  <button style={{ marginLeft: "10px" }} onClick={onJoin}>
-    Join
-  </button>
-);
-
-export default JoinButton;
