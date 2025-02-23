b8c6502d640628a4966b96c568638aacd0a21f2e
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-23 17:46:36 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './styles.css'
 import coreConceptsImg from "./assets/react-core-concepts.png";
+
+
+
+function Header() {
+  return (
+    <header>
+      {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
+      <img src={coreConceptsImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
 function App() {
   return (
     <div>
-      <header>
-        {/* <img src="src/assets/react-core-concepts.png" alt="Stylized atom" /> */}
-        <img src={coreConceptsImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
 
       <main>
         <h2>Time to get started!</h2>
