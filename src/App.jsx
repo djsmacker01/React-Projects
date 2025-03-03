4d910858b7f228d236ab519f525e156a9dee4357
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-03-03 11:01:40 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './styles.css'
-import { CORE_CONCEPTS } from './data';
-import Header from './components/Header/Header'; 
-import CoreConcept from './components/CoreConcept/CoreConcept';
+import "./styles.css";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 
-
-const contentDescriptions = ['Fundamentals', 'Crucial', 'Core']
+const contentDescriptions = ["Fundamentals", "Crucial", "Core"];
 function genRandom(max) {
-  return Math.floor(Math.random()* (max + 1))
+  return Math.floor(Math.random() * (max + 1));
 }
 function App() {
   return (
@@ -17,7 +16,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
@@ -29,4 +28,3 @@ function App() {
 }
 
 export default App;
-
