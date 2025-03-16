ed871be3e725a871203de0e1b12d58ea165f0657
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-03-16 16:05:22 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept/CoreConcept";
 import TabButton from "./components/CoreConcept/TabButton";
-import { EXAMPLES} from './dataContent'
+import { EXAMPLES } from "./dataContent";
 const contentDescriptions = ["Fundamentals", "Crucial", "Core"];
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState("components");
   // This should output a content for each button clicked
   function handleClick(selectedBtn) {
     // selectedBtn => 'components', 'jsx', 'props','state'
-    setSelectedTopic(selectedBtn)
+    setSelectedTopic(selectedBtn);
     // console.log(selectedTopic);
   }
   return (
@@ -39,13 +39,15 @@ function App() {
           </menu>
           {!selectedTopic ? <p>Please select a topic.</p> : null}
 
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : null}
         </section>
         {selectedTopic}
       </main>
