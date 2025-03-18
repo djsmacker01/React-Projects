692a33de7fb281f3425c4f037222b93a032d33b7
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-03-18 16:59:43 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,20 @@ function App() {
     setSelectedTopic(selectedBtn);
     // console.log(selectedTopic);
   }
+  let tabContent = <p>Please select a topic.</p>;
+  
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Header />
@@ -37,17 +51,7 @@ function App() {
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-          {!selectedTopic ? <p>Please select a topic.</p> : null}
-
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
+        
         </section>
         {/* {selectedTopic} */}
       </main>
